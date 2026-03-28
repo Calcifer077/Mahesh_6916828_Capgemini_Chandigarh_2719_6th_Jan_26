d687157c4caa2df6b0d677c7f276c820bbbb8ab1
Calcifer077 <maheshnashier14@gmail.com>
2026-03-28 16:01:13 +0530

    28 Mar Assessment

---
+++ Week12---23_Mar_26to28_Mar_26/28_Mar_26/EventBooking.API/Migrations/20260328050935_FixStaticSeedDates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventBooking.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStaticSeedDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2025, 10, 15, 9, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2025, 9, 20, 18, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2025, 11, 5, 10, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2026, 5, 28, 10, 37, 9, 608, DateTimeKind.Local).AddTicks(2307));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2026, 4, 28, 10, 37, 9, 609, DateTimeKind.Local).AddTicks(9506));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2026, 6, 28, 10, 37, 9, 609, DateTimeKind.Local).AddTicks(9547));
+        }
+    }
+}
